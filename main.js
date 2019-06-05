f62d3ed1ea2094cd3d3983c60b038024506c6d8b
Przemyslaw Szutowski <przemyslaw.szutowski@ivanti.com>
2019-06-05 16:55:48 +0200

    override default favicon

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: './favicon.ico',
     webPreferences: {
       nodeIntegration: true
     }
