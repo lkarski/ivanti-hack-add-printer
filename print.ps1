7ad9aa730acddd7cbf02958e7b8b700ff6b76cca
Robert Kilar <robert.kilar@ivanti.com>
2019-06-06 14:49:29 +0200

    10 to 20 seconds sleep

--- print.ps1
+++ print.ps1
@@ -1,4 +1,4 @@
 Param(
 [string] $Printer
 )
-$process = Start-Process -WindowStyle hidden -FilePath ".\assets\1.pdf" -Verb PrintTo $Printer -PassThru | %{sleep 10;$_} | kill
+$process = Start-Process -WindowStyle hidden -FilePath ".\assets\1.pdf" -Verb PrintTo $Printer -PassThru | %{sleep 20;$_} | kill
